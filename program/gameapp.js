1f4f9809dd9b27768d53c7608ccbf8f66afec1d9
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-02-26 19:49:52 +0000

    game round 1 functioning

--- program/gameapp.js
+++ program/gameapp.js
@@ -51,3 +51,66 @@ document.addEventListener('keydown', (event) => {
 });
 
 setAmmo();
+
+//Duck Logic
+
+let hitCounter = 0; // Initialize hit counter
+
+let duckCounter = document.getElementById("hit");
+
+
+
+let duck = document.getElementById('duck');
+let duckHit = false;
+let positionX = window.innerWidth * 0.25 + Math.random() * window.innerWidth * 0.5;
+let positionY = 0;
+let lastFrameTime = Date.now();
+let direction = Math.random() < 0.5 ? -1 : 1;
+
+function handleDuckHit() {
+    duck.style.display = "none"; 
+    console.log('Duck was hit');
+    hitCounter++;
+    duckHit = true;
+    let newDuckToCounter = document.createElement('img');
+    newDuckToCounter.src = "/resources/sprites/scoreImages/hit/duckwhite.png";
+    newDuckToCounter.className = "hudDuck";
+    duckCounter.appendChild(newDuckToCounter);
+    if (hitCounter === 10) {
+        alert('You win this round, increasing the difficulty!');
+    }
+}
+
+duck.addEventListener('click', handleDuckHit);
+
+function moveDuck() {
+    if (!duckHit) { // Add this line
+        duck.style.display = 'block';
+    }
+    let currentTime = Date.now();
+    let deltaTime = currentTime - lastFrameTime;
+    let buffer = 100; 
+    if (deltaTime >= 10) { 
+        positionY += 2; 
+        positionX += 2 * direction; 
+        duck.style.bottom = positionY + 'px';
+        duck.style.left = positionX + 'px';
+        if (positionY >= window.innerHeight + buffer || positionX < -buffer || positionX >= window.innerWidth + buffer) { 
+            positionY = 0; 
+            positionX = window.innerWidth * 0.25 + Math.random() * window.innerWidth * 0.5; 
+            direction = positionX > window.innerWidth / 2 ? -1 : 1; 
+            duckHit = false;
+            duck.removeEventListener('click', handleDuckHit); 
+            duck.addEventListener('click', handleDuckHit); 
+        }
+        lastFrameTime = currentTime;
+    }
+    requestAnimationFrame(moveDuck);
+}
+
+setTimeout(() => {
+    requestAnimationFrame(moveDuck); 
+}, 5000); 
+
+
+// Game Start logic so the duck doesn't start moving until the game starts
