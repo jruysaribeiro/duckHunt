03b38295315e48817966c13cbb3bebd8a4346850
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-02-26 14:57:00 +0000

    html and css for menu and settings and pause menu

--- program/menuapp.js
+++ program/menuapp.js
@@ -13,3 +13,51 @@ settingControls.addEventListener('click', () => {
     document.getElementById('menu').style.display = 'none';
     document.getElementById('settingsView').style.display = 'block';
 });
+
+const pauseControl = document.getElementById('game');
+
+        // pause the game
+ document.addEventListener('keydown', (event) => {
+        if(document.getElementById('pause').style.display === 'block') {
+            if(event.key === 'q') {
+                console.log('resumed');
+                document.getElementById('pause').style.display = 'none';
+                document.getElementById('game').style.display = 'flex';
+            }
+            return;
+        }
+        console.log("paused")
+        if(event.key === 'q') {
+        document.getElementById('game').style.display = 'none';
+        document.getElementById('pause').style.display = 'block';
+    }
+});
+
+const exitControls = document.getElementById('exit');
+
+        // exit the game on pause menu
+exitControls.addEventListener('click', () => {
+    console.log('exit clicked');
+    document.getElementById('pause').style.display = 'none';
+    document.getElementById('menu').style.display = 'flex';
+});
+
+const backButton = document.getElementById('back');
+
+backButton.addEventListener('click', () => {
+    console.log('back clicked');
+    document.getElementById('settingsView').style.display = 'none';
+    document.getElementById('menu').style.display = 'flex';
+}
+);
+
+const resumeButton = document.getElementById('resume');
+
+resumeButton.addEventListener('click', () => {
+    console.log('resume clicked');
+    document.getElementById('pause').style.display = 'none';
+    document.getElementById('game').style.display = 'flex';
+}
+);
+
+
